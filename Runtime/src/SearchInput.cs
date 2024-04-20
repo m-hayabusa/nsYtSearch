6f7c54be5537d3529ec7e3d2efd4c5fba7a876e4
m-hayabusa <s.hashimoto@hs-sh.net>
2024-04-20 13:07:30 +0900

    検索キーワードから検索URLをつくるパネルを追加

---
+++ Runtime/src/SearchInput.cs
@@ -0,0 +1,20 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SearchInput : UdonSharpBehaviour
+{
+    private InputField src;
+    [SerializeField] private InputField dest;
+    void Start()
+    {
+        src = GetComponent<InputField>();
+        dest = transform.parent.Find("SearchInputResult").GetComponent<InputField>();
+    }
+    public void OnEndEdit()
+    {
+        dest.text = "https://www.youtube.com/results?search_query=" + src.text.Replace(" ", "+").Replace("?", "%3F").Replace("&", "%26");
+        dest.ActivateInputField();
+    }
+}
